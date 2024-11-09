11033fa7b27e9cbcae8bc6f498b667b40e963f77
PiotrW01 <piotr140401@gmail.com>
2024-11-09 03:01:20 +0100

    JSON support added

--- GodotClient/main.gd
+++ GodotClient/main.gd
@@ -3,17 +3,18 @@ extends Node2D
 var socket = WebSocketPeer.new()
 const URL = "ws://localhost:3000"
 
-
 func _ready():
 	socket.connect_to_url(URL)
-
 
 func _process(delta):
 	socket.poll()
 	var state = socket.get_ready_state()
 	if state == WebSocketPeer.STATE_OPEN:
 		while socket.get_available_packet_count():
-			print("Packet: ", socket.get_packet().get_string_from_ascii())
+			var json = JSON.parse_string(socket.get_packet().get_string_from_ascii())
+			print(json)
+			
+			
 	elif state == WebSocketPeer.STATE_CLOSING:
 		pass
 	elif state == WebSocketPeer.STATE_CLOSED:
@@ -24,7 +25,12 @@ func _process(delta):
 
 
 func _on_button_down():
-	socket.send_text("message 9423")
+	var array = PackedByteArray()
+	var val = 0x89
+	array.append(val)
+	array.append(0x00)
+	socket.put_packet(array)
+	#socket.send_text("message 9423")
 
 
 func _on_reconnect_button_down():
--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,15 @@
 import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({"port": 3000});
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
     console.log("A connection established")
-
+    
     ws.on('error', console.error);
-    ws.on('open', () => {
-        console.log("Connected!")
-    })
     ws.on('message', function message(data) {
         const decoder = new TextDecoder();
         console.log('received: ', decoder.decode(data));
     })
-
-    ws.send('something');
+    const obj = {"test": "aaaa"};
+    const str = JSON.stringify(obj);
+    ws.send(new TextEncoder().encode(str));
 });
-
